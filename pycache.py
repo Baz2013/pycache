43fdd3428a7052d6d671b509de20b13597e9a853
gucb <chuan.gu6@gmail.com>
2016-10-31 16:13:02 +0800

    modified:   .gitignore 	modified:   pycache.py 	modified:   test_pycache.py

--- pycache.py
+++ pycache.py
@@ -2,6 +2,28 @@
 
 # 实现简单的缓存功能
 
+import sqlite3
+import datetime
+import time
+
+
+class Data(object):
+    """
+    数据存储结构
+    """
+
+    def __init__(self, r_key, r_val, r_expire_time):
+        """
+        :param r_key:
+        :param r_val:
+        :param r_expire_time: 将要过期的时间,精确到秒
+        :return:
+        """
+        self.key = r_key
+        self.val = r_val
+        self.expire_time = r_expire_time
+
+
 class PyCache(object):
     """
     # 实现简单的缓存功能
@@ -23,14 +45,20 @@ class PyCache(object):
             raise 'StoreError', '存储方式设置错误'
 
         self.store = r_store
+        if self.store_type == 'db':
+            self.table = 't_' + self.store[:-3]
+            self.db_conn = self._create_db()
 
-    def set(self, r_kv, r_expire=-1):
+    def set(self, r_key, r_val, r_expire=-1):
         """
         设置键值对
-        :param r_kv: 键值对
+        :param r_key: 键
+        :param r_val: 值
         :param r_expire:过期时间,如果过期时间为负值则持久存储
         :return:
         """
+        if self.store_type == 'db':
+            self._set_db_data(r_key, r_val, r_expire)
 
     def get(self, r_key):
         """
@@ -38,3 +66,112 @@ class PyCache(object):
         :param r_key:
         :return:
         """
+        if self.store_type == 'db':
+            return self._get_db_data(r_key)
+
+    def _create_db(self):
+        """
+        创建db文件,及存储表
+        :return:
+        """
+        conn = sqlite3.connect(self.store)
+        cursor = conn.cursor()
+        create_sql = 'create table if not exists %s (key varchar(64), val varchar(64), expire_time varchar(16))' % (
+            self.table,)
+        cursor.execute(create_sql)
+
+        return conn
+
+    def _set_db_data(self, r_key, r_val, r_expire):
+        """
+        :param r_key:
+        :param r_val:
+        :param r_expire:
+        :return:
+        """
+        expire_time = self._get_expire_time(r_expire)
+        cursor = self.db_conn.cursor()
+        if self._is_key_exists(r_key):
+            sql = "update {0} set val = '{1}', expire_time = '{2}' where key = '{3}' ".format(self.table, r_val,
+                                                                                              expire_time, r_key)
+        else:
+            sql = "insert into {0} values ('{1}','{2}','{3}')".format(self.table, r_key, r_val, expire_time)
+        print sql
+        cursor.execute(sql)
+        self.db_conn.commit()
+        cursor.close()
+
+    def _get_db_data(self, r_key):
+        """
+        :param r_key:
+        :return:
+        """
+        if not self._is_key_exists(r_key):
+            return
+        sql = "select val,expire_time from %s where key = '%s' " % (self.table, r_key)
+        cursor = self.db_conn.cursor()
+        cursor.execute(sql)
+        res = cursor.fetchone()
+        val = res[0]
+        expire_time = int(res[1])
+        if expire_time > self._get_expire_time(0):
+            cursor.close()
+            return val
+        else:
+            self._del_key(r_key)
+            return
+
+    def _del_key(self, r_key):
+        """
+        :param r_key:
+        :return:
+        """
+
+        cursor = self.db_conn.cursor()
+        del_sql = "delete from %s where key = '%s'" % (self.table, r_key)
+        cursor.execute(del_sql)
+        self.db_conn.commit()
+        cursor.close()
+
+    def _get_expire_time(self, r_expire):
+        """
+        :param r_expire:
+        :return:
+        """
+        dela_secs = datetime.timedelta(seconds=r_expire)
+        expire_time = datetime.datetime.now() + dela_secs
+
+        return int(time.mktime(expire_time.timetuple()))
+
+    def _is_key_exists(self, r_key):
+        """
+        :param r_key:
+        :return:
+        """
+        sql = "select count(*) from %s where key = '%s' " % (self.table, r_key)
+        cursor = self.db_conn.cursor()
+        cursor.execute(sql)
+        res = cursor.fetchone()
+        cursor.close()
+        # print res
+        return res[0]
+
+    def ttl(self, r_key):
+        """
+        :param r_key:
+        :return:
+        """
+        if not self._is_key_exists(r_key):
+            return
+        sql = "select val,expire_time from %s where key = '%s' " % (self.table, r_key)
+        cursor = self.db_conn.cursor()
+        cursor.execute(sql)
+        res = cursor.fetchone()
+        expire_time = int(res[1])
+        cursor.close()
+        cur_time = self._get_expire_time(0)
+        if expire_time > cur_time:
+            return expire_time - cur_time
+        else:
+            self._del_key(r_key)
+            return 0
--- test_pycache.py
+++ test_pycache.py
@@ -1,4 +1,33 @@
 # -*- coding:utf-8 -*-
 
 import unittest
+from pycache import *
+import time
 
+
+class CacheTest(unittest.TestCase):
+    def setUp(self):
+        self.ins = PyCache('cache.db')
+
+    def test_set(self):
+        """
+        :return:
+        """
+        self.ins.set('name', 'gucb', 20)
+        self.ins.set('name1', 'liu', 60 * 10)
+
+    def test_get_expire_time(self):
+        self.assertEquals(self.ins._get_expire_time(20), self.ins._get_expire_time(0) + 20)
+
+    def test_get(self):
+        print self.ins.get('name')
+
+    def test_ttl(self):
+        self.ins.set('name2', 'lllll', 10)
+        time.sleep(10)
+        self.assertEquals(0, self.ins.ttl('name2'))
+
+    def test_ttl_1(self):
+        self.ins.set('name3', 'ssss', 20)
+        time.sleep(5)
+        self.assertEquals(15, self.ins.ttl('name3'))
